856160e1965a5dee5b25f50bbd47cd1ee3cfc77d
Krist Wongsuphasawat <kristw@twitter.com>
2014-03-11 15:55:11 -0700

    fix sign filter

--- build/angular-knife.js
+++ build/angular-knife.js
@@ -21,8 +21,7 @@ filters.filter('positiveNegative', function () {
 
 filters.filter('sign', function () {
   return function(input) {
-    input = +input;
-    return (input > 0)? ("+" + input) : input;
+    return (+input > 0)? ("+" + input) : input;
   };
 });
 
--- src/filters/sign.js
+++ src/filters/sign.js
@@ -1,6 +1,5 @@
 filters.filter('sign', function () {
   return function(input) {
-    input = +input;
-    return (input > 0)? ("+" + input) : input;
+    return (+input > 0)? ("+" + input) : input;
   };
 });
